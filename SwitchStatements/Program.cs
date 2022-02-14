8a50f0be7b858be018bbb3604d9bccfb9dcbd3eb
Devan <deshlock010101@hotmai.com>
2022-02-14 15:49:21 -0600

    finished the assignment.

--- SwitchStatements/Program.cs
+++ SwitchStatements/Program.cs
@@ -6,7 +6,31 @@ namespace SwitchStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("What is your favorite subject in school?: ");
+            var favSubject = Console.ReadLine();
+            
+
+            switch(favSubject)
+            {
+                case "computer Science":
+                    Console.WriteLine("Computer Science is super hard! ");
+                    break;
+                case "math":
+                    Console.WriteLine("You can really go places with math. ");
+                    break;
+                case "english":
+                    Console.WriteLine("English was always my least favorite subject ");
+                    break;
+                case "history":
+                    Console.WriteLine("I have come to really like history in the past few years. ");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is the best!! ");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that subject. ");
+                    break;
+            }
         }
     }
 }
